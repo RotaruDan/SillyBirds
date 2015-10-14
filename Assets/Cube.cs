d9044edc30fbea91e5761bb01fb622d5e361f2c1
RotaruDan <drotaru@ucm.es>
2015-10-14 13:24:59 +0200

    Improves the Reynolds algorithm implementation.

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -30,8 +30,15 @@ public class Cube : MonoBehaviour
 
     void Update()
     {
+        Vector3 v1, v2, v3;
+        v1 = rule1(this);
+        v2 = rule2(this);
+        v3 = rule3(this);
+
+        newVelocity = velocity + v1 + v2 + v3;
+
         // Initialize newVelocity and newPosition to the current values
-        newVelocity = velocity;
+        //newVelocity = velocity;
         newPosition = this.transform.position;
         Vector3 randomVel = Random.insideUnitSphere;
         // Utilizes the fields set on the CubeSpawner.C Singleton
@@ -51,19 +58,107 @@ public class Cube : MonoBehaviour
     }
 
     void LateUpdate()
-    {
+    {        
         // Adjust the current velocity based on newVelocity using a linear
         // interpolation
         velocity = (1 - CubeSpawner.C.velocityLerpAmt) * velocity +
         CubeSpawner.C.velocityLerpAmt * newVelocity;
+
         // Decide on the newPosition
-        newPosition = this.transform.position + velocity * Time.deltaTime;
+        newPosition = this.transform.position + this.velocity * Time.deltaTime;
+
         // Keep everything in the XZ plane
         newPosition.y = 0;
         // Look from the old position at the newPosition to orient the model
         this.transform.LookAt(newPosition);
         // Actually move to the newPosition
         this.transform.position = newPosition;
+        
+
     }
 
+    /**
+        PROCEDURE rule1(boid bJ)
+            Vector pcJ
+            FOR EACH BOID b
+                IF b != bJ THEN
+                pcJ = pcJ + b.position
+                END IF
+            END
+            pcJ = pcJ / N-1
+            RETURN (pcJ - bJ.position) / 100
+        END PROCEDURE
+    **/
+    Vector3 rule1(Cube bJ)
+    {
+        Vector3 pcJ = Vector3.zero;
+        foreach (Cube b in CubeSpawner.C.cubes)
+        {
+            if (b != bJ)
+            {
+                pcJ = pcJ + b.transform.position;
+            }
+        }
+        pcJ = pcJ / (CubeSpawner.C.cubes.Count - 1);
+
+        return (pcJ - bJ.transform.position) / 100;
+    }
+
+    /**
+        PROCEDURE rule2(boid bJ)
+            Vector c = 0;
+            FOR EACH BOID b
+                IF b != bJ THEN
+                    IF b.position - bJ.position lt 100 THEN
+                        c = c - (b.position - bJ.position)
+                    END IF
+                END IF
+            END
+            RETURN c
+        END PROCEDURE
+    **/
+    Vector3 rule2(Cube bJ)
+    {
+        Vector3 c = Vector3.zero;
+        foreach (Cube b in CubeSpawner.C.cubes)
+        {
+            if (b != bJ)
+            {
+                Vector3 len = b.transform.position - bJ.transform.position;
+                if (len.magnitude < 2)
+                {
+                    c = c - (b.transform.position - bJ.transform.position);
+                }
+            }
+        }
+        return c;
+    }
+
+    /**
+        PROCEDURE rule3(boid bJ)
+            Vector pvJ
+            FOR EACH BOID b
+                IF b != bJ THEN
+                    pvJ = pvJ + b.velocity
+                END IF
+            END
+            pvJ = pvJ / N-1
+            RETURN (pvJ - bJ.velocity) / 8
+        END PROCEDURE
+    **/
+    Vector3 rule3(Cube bJ)
+    {
+        Vector3 pvJ = Vector3.zero;
+        foreach (Cube b in CubeSpawner.C.cubes)
+        {
+            if (b != bJ)
+            {
+                pvJ = pvJ + b.velocity;
+            }
+        }
+        pvJ = pvJ / (CubeSpawner.C.cubes.Count - 1);
+
+        return (pvJ - bJ.velocity) / 8;
+    }
+    
 }
--- Assets/CubeSpawner.cs
+++ Assets/CubeSpawner.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 
 public class CubeSpawner : MonoBehaviour
 {
-    static public List<Cube> cubes = new List<Cube>();
     static public CubeSpawner C;
-    public float velocityMatchingAmt = 0.01f;
-    public float velocityLerpAmt = 0.2f;
+
+    public List<Cube> cubes = new List<Cube>();
+    public float velocityMatchingAmt = 0.1f;
+    public float velocityLerpAmt = 0.3f;
     public GameObject cubePrefab;
-    public int numCubes = 25;
+    public int numCubes = 50;
     public float spawnRadius = 100f;
 
-    public float mouseAttractionAmt = 0.03f;
-    public float mouseAvoidanceAmt = 0.55f;
-    public float mouseAvoidanceDist = 40f;
+    public float mouseAttractionAmt = 0.01f;
+    public float mouseAvoidanceAmt = 20f;
+    public float mouseAvoidanceDist = 20f;
     public Vector3 mousePos;
 
     // Use this for initialization
@@ -26,151 +27,9 @@ public class CubeSpawner : MonoBehaviour
         }
     }
 
-    /**
-        LOOP
-            draw_boids()
-            move_all_boids_to_new_positions()
-        END LOOP
-    **/
     // Update is called once per frame
     void Update()
     {
-        move_all_boids_to_new_positions();
-    }
-
-    /**
-        PROCEDURE move_all_boids_to_new_positions()
-            Vector v1, v2, v3
-            Boid b
-            FOR EACH BOID b
-                v1 = rule1(b)
-                v2 = rule2(b)
-                v3 = rule3(b)
-                b.velocity = b.velocity + v1 + v2 + v3
-                b.position = b.position + b.velocity
-            END
-        END PROCEDURE
-    **/
-    void move_all_boids_to_new_positions()
-    {
-        Vector3 v1, v2, v3;
-        
-        foreach(Cube cube in cubes)
-        {
-            v1 = rule1(cube);
-            v2 = rule2(cube);
-            v3 = rule3(cube);
-
-            cube.velocity = cube.velocity + v1 + v2 + v3;
-            cube.transform.position = cube.transform.position + cube.velocity;
-        }
-
-    }
-
-    /**
-        PROCEDURE rule1(boid bJ)
-            Vector pcJ
-            FOR EACH BOID b
-                IF b != bJ THEN
-                pcJ = pcJ + b.position
-                END IF
-            END
-            pcJ = pcJ / N-1
-            RETURN (pcJ - bJ.position) / 100
-        END PROCEDURE
-    **/
-    Vector3 rule1(Cube bJ)
-    {
-        Vector3 pcJ = Vector3.zero;
-        foreach(Cube b in cubes)
-        {
-            if(b != bJ)
-            {
-                pcJ = pcJ + b.transform.position;
-            }
-        }
-        pcJ = pcJ / (cubes.Count - 1);
-
-        return (pcJ - bJ.transform.position) / 100;
-    }
-
-    public Vector3 GetAveragePosition(List<Cube> someBoids)
-    {
-        Vector3 sum = Vector3.zero;
-        foreach (Cube b in someBoids)
-        {
-            sum += b.transform.position;
-        }
-        Vector3 center = sum / someBoids.Count;
-        return (center);
-    }
-
-    /**
-        PROCEDURE rule2(boid bJ)
-            Vector c = 0;
-            FOR EACH BOID b
-                IF b != bJ THEN
-                    IF b.position - bJ.position lt 100 THEN
-                        c = c - (b.position - bJ.position)
-                    END IF
-                END IF
-            END
-            RETURN c
-        END PROCEDURE
-    **/
-    Vector3 rule2(Cube bJ)
-    {
-        Vector3 c = Vector3.zero;
-        foreach(Cube b in cubes)
-        {
-            if(b != bJ)
-            {
-                Vector3 len = b.transform.position - bJ.transform.position;
-                if (len.magnitude < 100)
-                {
-                    c = c - (b.transform.position - bJ.transform.position);
-                }
-            }
-        }
-        return c;
-    }
-
-    /**
-        PROCEDURE rule3(boid bJ)
-            Vector pvJ
-            FOR EACH BOID b
-                IF b != bJ THEN
-                    pvJ = pvJ + b.velocity
-                END IF
-            END
-            pvJ = pvJ / N-1
-            RETURN (pvJ - bJ.velocity) / 8
-        END PROCEDURE
-    **/
-    Vector3 rule3(Cube bJ)
-    {
-        Vector3 pvJ = Vector3.zero;
-        foreach (Cube b in cubes)
-        {
-            if(b != bJ)
-            {
-                pvJ = pvJ + b.velocity;
-            }
-        }
-        pvJ = pvJ / (cubes.Count - 1);
-
-        return (pvJ - bJ.velocity) / 80;
-    }
-
-    public Vector3 GetAverageVelocity(List<Cube> someBoids)
-    {
-        Vector3 sum = Vector3.zero;
-        foreach (Cube b in someBoids)
-        {
-            sum += b.velocity;
-        }
-        Vector3 avg = sum / someBoids.Count;
-        return (avg);
     }
 
     void LateUpdate()
